b5f80f70dfbf1295e7e0ca8b342b9da08db79b92
ZimovOleg <zimov.oleg@gmail.com>
2021-12-26 17:08:26 +0400

    Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int eaters = 5; // сколько людей будут есть
+        int ticket = 101; // Стоимость билетов
+        int payment = 20; // расчет бонусных миль
+        int bonus; // кол-во бонусных миль
 
-        int water = 3000; // миллилитров воды
-        int potatoes = 5; // картофелин
-        int chicken = 6; // куриных бёдер
-        int spices = 10; // ложек специй
-
-        System.out.println("Сварили суп. На одного человека вышло:");
-        System.out.println((water / eaters) + " миллилитров воды");
-        System.out.println((potatoes / eaters) + " картофелин(а)");
-        System.out.println((chicken / eaters) + " куриных(ое) бёдер(ро)");
-        System.out.println((spices / eaters) + " ложек(ка) специй");
+        if (ticket > 20) { bonus = (ticket / payment);
+            System.out.println("Поздравляем Вы получили:");
+            System.out.println(bonus + " бонусных миль");}
+        else {        System.out.println("Бонусные мили не начисленны");
+        }
 
     }
 }
